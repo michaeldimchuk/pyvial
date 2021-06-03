477542b1ea6ef286fc12206bbe6798b7839fe629
Michael Dimchuk <michaeldimchuk@gmail.com>
2021-06-02 22:34:26 -0700

    Added sanity checks for errors

---
+++ tests/unit/test_errors.py
@@ -0,0 +1,28 @@
+from http import HTTPStatus
+from typing import Type
+
+import pytest
+
+from vial.errors import (
+    BadRequestError,
+    ForbiddenError,
+    MethodNotAllowedError,
+    NotFoundError,
+    ServerError,
+    UnauthorizedError,
+)
+
+
+@pytest.mark.parametrize(
+    "error, status",
+    [
+        (BadRequestError, HTTPStatus.BAD_REQUEST),
+        (ForbiddenError, HTTPStatus.FORBIDDEN),
+        (MethodNotAllowedError, HTTPStatus.METHOD_NOT_ALLOWED),
+        (NotFoundError, HTTPStatus.NOT_FOUND),
+        (ServerError, HTTPStatus.INTERNAL_SERVER_ERROR),
+        (UnauthorizedError, HTTPStatus.UNAUTHORIZED),
+    ],
+)
+def test_status(error: Type[ServerError], status: HTTPStatus) -> None:
+    assert error.status == status
--- vial/errors.py
+++ vial/errors.py
@@ -5,6 +5,18 @@ class ServerError(Exception):
     status = HTTPStatus.INTERNAL_SERVER_ERROR
 
 
+class BadRequestError(ServerError):
+    status = HTTPStatus.BAD_REQUEST
+
+
+class UnauthorizedError(ServerError):
+    status = HTTPStatus.UNAUTHORIZED
+
+
+class ForbiddenError(ServerError):
+    status = HTTPStatus.FORBIDDEN
+
+
 class NotFoundError(ServerError):
     status = HTTPStatus.NOT_FOUND
 
